619a1dff2e2a2bbf07df7f83511feb903db01c81
sadhvik <46477311+sadhvik@users.noreply.github.com>
2025-08-03 16:36:02 +0530

    Delete Week-7 directory

--- Week-7/Week-7/10.ReactJS-HOL/components/Heading.js
+++
@@ -1,7 +0,0 @@
-function Heading() {
-    return (
-        <h1>Office Space At Affordable Rates!</h1>
-    )
-}
-
-export default Heading;
--- Week-7/Week-7/13.ReactJS-HOL/src/courses.js
+++
@@ -1,4 +0,0 @@
-export const courses = [
-  { id: 1, name: "Angular", date: "4/5/2021" },
-  { id: 2, name: "React", date: "6/3/2021" },
-];
